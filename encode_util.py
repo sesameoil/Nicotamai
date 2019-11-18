fe8542fa82ad07651e2952ffcc41f7892e6f3e78
sesameoil <honyamato@gmail.com>
2019-11-19 00:22:58 +0900

    intial

---
+++ encode_util.py
@@ -0,0 +1,84 @@
+def __bytes_to_2bits(byts):
+    bits = []
+    for b in byts:
+        for mask, shift in zip((0b11000000, 0b110000, 0b1100, 0b11), (6, 4, 2, 0)):
+            bits.append((b & mask) >> shift)
+            """
+            print(bin(b))
+            print(bin(mask), shift)
+            print(bin(b & mask))
+            print(bin((b & mask) >> shift))
+            print(bits)
+            print("=============")"""
+    return tuple(bits)
+
+
+def __bits_to_bytes(bits):
+    str_bits = ""
+    for b in bits:
+        if b == 0:
+            str_bits += '00'
+        if b == 1:
+            str_bits += '01'
+        if b == 2:
+            str_bits += '10'
+        if b == 3:
+            str_bits += '11'
+    hex_bits = format(int(str_bits, base=2), 'x')
+    if len(hex_bits) % 2 == 1:
+        hex_bits = "0"+hex_bits
+    byts = bytes.fromhex(hex_bits)
+    byts = b"\x00" * str_bits.count("00000000", 0,
+                                    len(str_bits)-len(str_bits.lstrip("0"))) + byts
+    return byts
+
+
+def encode(byts):
+    bits = __bytes_to_2bits(byts)
+    bits_encoded = ""
+    for b in bits:
+        if b == 0:
+            bits_encoded += '二'
+        if b == 1:
+            bits_encoded += '子'
+        if b == 2:
+            bits_encoded += '玉'
+        if b == 3:
+            bits_encoded += '舞'
+    return bits_encoded
+
+
+def decode(strings):
+    bits = []
+    for s in strings:
+        if s == '二':
+            bits.append(0)
+        if s == '子':
+            bits.append(1)
+        if s == '玉':
+            bits.append(2)
+        if s == '舞':
+            bits.append(3)
+    byts = __bits_to_bytes(bits)
+    return byts
+
+
+if __name__ == '__main__':
+    a = __bytes_to_2bits(b'a')
+    hoge = ""
+    for x in a:
+        if x == 0:
+            hoge += '00'
+        if x == 1:
+            hoge += '01'
+        if x == 2:
+            hoge += '10'
+        if x == 3:
+            hoge += '11'
+    testbytes = b'\x00\x00\x00\x1f\xffabc2\x00\x00\x00\x02\x00\x00\x00'
+    print("hoge", hoge)
+    print(encode(testbytes))
+    decoded = decode(encode(testbytes))
+    print(decoded)
+    print(decoded == testbytes)
+    print(encode(b'a'))
---
+++ main.py
@@ -0,0 +1,37 @@
+import encode_util
+import argparse
+prog_name = "Nicotamai"
+version = "0.1.0"
+writer = "sesameoil"
+license = "CC0 1.0 Universal. See Also LICENSE, or "\
+          "<https://creativecommons.org/publicdomain/zero/1.0/legalcode>"
+
+
+parser = argparse.ArgumentParser(prog=prog_name,
+                                 description="Nicotamai encode or decode"
+                                 "IN_FILE, or standard input,"
+                                 "to OUT_FILE, or standard output.",
+                                 usage="usage: %(prog)s [OPTION]... [IN_FILE] [OUT_FILE]",
+                                 epilog=f"Written by {writer}.\nLicence: {license}.",
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('-d', '--decode', action='store_true',
+                    help='decode data')
+parser.add_argument('IN_FILE', type=argparse.FileType('r'),
+                    help='Input. When IN_FILE is -, read standard input')
+parser.add_argument('OUT_FILE', type=argparse.FileType('w'),
+                    help='Output. When OUT_FILE is -, write standard output')
+parser.add_argument('-v', '--version', action='version',
+                    version=(f"%(prog)s {version}\n\nWritten by {writer}.\nLicence: {license}."))
+
+args = parser.parse_args()
+if args.decode:
+    with args.IN_FILE.read() as nicotamai:
+        byts = encode_util.decode(nicotamai)
+        with args.OUT_FILE.buffer as buffer_OUT_FILE:
+            buffer_OUT_FILE.write(byts)
+else:
+    with args.IN_FILE.buffer as buffer_IN_FILE:
+        byts = buffer_IN_FILE.read()
+        with args.OUT_FILE as OUT_FILE:
+            nicotamai = encode_util.encode(byts)
+            OUT_FILE.write(nicotamai + '\n')
